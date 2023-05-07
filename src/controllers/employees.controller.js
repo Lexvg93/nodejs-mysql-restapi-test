040cb94fc5ba1310edd1b1ee097a4bec8f71ca35
Lexvg93 <alejandro_gonzalo92@icloud.com>
2023-05-07 17:29:37 +0200

    avance 17:29

---
+++ src/controllers/employees.controller.js
@@ -0,0 +1,14 @@
+import {pool} from '../db.js'
+
+export const getEmployees = (req,res) => res.send('obteniendo empleados');
+
+export const createEmployee = async(req,res) => {
+    const {name, salary} = req.body
+ const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)',[name, salary])
+ 
+ res.send({rows})
+}
+
+export const updateEmployee = (req,res) => res.send('actualizando empleados');
+
+export const deleteEmployee = (req,res) => res.send('eliminando empleados');
