067d1a48aed3ba8e877849b2cb85b151460fdd93
Lexvg93 <alejandro_gonzalo92@icloud.com>
2023-05-07 18:04:45 +0200

    avance 18:04

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,12 +1,26 @@
 import {pool} from '../db.js'
 
-export const getEmployees = (req,res) => res.send('obteniendo empleados');
+export const getEmployees = async(req,res) => {
+  const [rows] = await pool.query('SELECT * FROM employee')
+  res.json(rows)
+}
+
+export const getEmployee = async (req,res) => {
+    const {id} = req.params
+    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
+    console.log(rows)
+    res.json(rows[0])
+}
 
 export const createEmployee = async(req,res) => {
     const {name, salary} = req.body
- const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)',[name, salary])
+    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)',[name, salary])
  
- res.send({rows})
+     res.send({
+        id: rows.insertId,
+        name,
+        salary,
+    })
 }
 
 export const updateEmployee = (req,res) => res.send('actualizando empleados');
