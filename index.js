8474ca4ceb11753ab54608469fb698560ad07a06
Lexvg93 <alejandro_gonzalo92@icloud.com>
2023-05-07 16:30:05 +0200

    avance 16:29

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const app = express()
 
 
 app.get('/employees',(req,res) => res.send('obteniendo empleados'))
+
 app.post('/employees',(req,res) => res.send('creando empleados'))
+
 app.put('/employees',(req,res) => res.send('actualizando empleados'))
+
 app.delete('/employees',(req,res) => res.send('eliminando empleados'))
+
 app.listen(3000)
 console.log('Server running on port 3000')
