d6b3132a8bb750ab23b67f20f60b56581780ed89
Lexvg93 <alejandro_gonzalo92@icloud.com>
2023-05-08 11:27:29 +0200

    avance lunes pausa 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
+import './config.js'
 const app = express()
 
 app.use(express.json())
