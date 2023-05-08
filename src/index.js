c7ffe9e60f59cfc34317258871c495a97720d503
Lexvg93 <alejandro_gonzalo92@icloud.com>
2023-05-08 15:09:12 +0200

    avance lunes 15:09

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
-import './config.js'
+import {PORT} from './config.js'
 const app = express()
 
 app.use(express.json())
@@ -15,5 +15,5 @@ app.use((req, res, next) => {
     })
 })
 
-app.listen(3000)
-console.log('Server running on port 3000')
+app.listen(PORT)
+console.log('Server running on port', PORT)
